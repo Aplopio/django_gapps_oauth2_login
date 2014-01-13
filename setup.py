e200aab89a7d16b90f57516a89dbd99dbef359f1
vivek <vivek@aplopio.com>
2014-01-13 17:24:07 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+
+long_description="""
+django_gapps_oauth2_login
+=========================
+
+git clone https://github.com/Aplopio/django_gapps_oauth2_login.git
+
+Download client secrets in json provided by google cloud console credentials & rename to client_secrets.json in this directory.
+
+Add 'django_gapps_oauth2_login' to INSTALLED_APPS in settings.py
+
+Update urls.py by adding following entry:
+
+(r'^oauth2/', include('django_gapps_oauth2_login.urls'))
+
+run python manage.py syncdb
+
+Write custom receivers for user_created_via_oauth2 & redirect_user_loggedin_via_oauth2 signals
+"""
+
+setup(name='django_gapps_oauth2_login',
+      version='0.1',
+      description='Django Google Apps Oauth2 Logi',
+      long_description = long_description,
+      author='Vivek Chand',
+      author_email='vivek@aplopio.com',
+      url='https://github.com/Aplopio/django_gapps_oauth2_login',
+      classifiers = [
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+      ],
+      keywords = ["django ", "query", "search"],
+      packages = ['django_gapps_oauth2_login'],
+      install_requires = ['google-api-python-client==1.2'],        
+      )
