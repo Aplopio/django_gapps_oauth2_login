a342175136dbdf572dfb1977d2968d2978c38d9e
vivek <vivek@aplopio.com>
2014-01-15 17:52:56 +0530

    Added related name for oauth2 user

--- django_gapps_oauth2_login/models.py
+++ django_gapps_oauth2_login/models.py
@@ -5,10 +5,10 @@ from oauth2client.django_orm import CredentialsField
 
 
 class CredentialsModel(models.Model):
-  id = models.ForeignKey(User, primary_key=True)
+  id = models.ForeignKey(User, primary_key=True, related_name="user_credential")
   credential = CredentialsField()
 
 class UserOauth2(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, related_name="oauth2_user")
     claimed_id = models.TextField(max_length=2047)
     display_id = models.TextField(max_length=2047)
