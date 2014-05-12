8199c5d20c4d34bb548fbe75310f58a52987c83e
Vivek Chand <vivek@aplopio.com>
2014-05-12 17:00:52 +0530

    Bumped version no

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ Run testcases, python manage.py test django_gapps_oauth2_login
 """
 
 setup(name='django_gapps_oauth2_login',
-      version='0.9.7.3',
+      version='0.9.7.4',
       description='Django Google Apps Oauth2 Login',
       long_description = long_description,
       author='Vivek Chand',
