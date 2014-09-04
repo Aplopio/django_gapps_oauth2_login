f98622b3bcce82190b8509dc60c17db406511965
Vignesh Sarma K <vignesh@recruiterbox.com>
2014-09-04 19:03:01 +0530

    get rid of the use of url.default

--- django_gapps_oauth2_login/urls.py
+++ django_gapps_oauth2_login/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('django_gapps_oauth2_login.views',
                        url(r'^login/$', 'login_begin'),
