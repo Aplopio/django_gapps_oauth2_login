c5b8933768c114e4f66e8194a0b1fb2e437f102c
Anant Pal <anantkpal@yahoo.co.in>
2017-01-17 14:23:32 +0530

    bump version 0.9.8.2b => 0.9.8.2c

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if needs_json:
   install_requires.append('simplejson')
 
 setup(name='django_gapps_oauth2_login',
-      version='0.9.8.2b',
+      version='0.9.8.2c',
       description='Django Google Apps Oauth2 Login',
       long_description = long_description,
       author='Vivek Chand',
